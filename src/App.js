e0eb59df4a187b0369e482a873f448595f64b051
איתי <@DESKTOP-5CAMIR6>
2024-01-14 22:19:33 +0200

    Topic Page and Sub Topic Page

--- src/App.js
+++ src/App.js
@@ -1,50 +1,19 @@
 
-import './App.css';
-import Buttons from './MatirealUi/Buttons';
+import background from './img/background.jpg'
+import TopicsPage from './components/TopicsPage';
 
-
-const addTwoNumbers = (list1, list2) => {
-  let mulNum = 1;
-  let num1 = 0, num2 = 0;
-  let result;
-  let arr =[];
-
-  list1.forEach(node => {
-    node *= mulNum;
-    num1 += node;
-    mulNum *= 10
-  });
-
-  mulNum = 1;
-
-  list2.forEach(node => {
-    node *= mulNum;
-    num2 += node;
-    mulNum *= 10
-  });
-
-  result = num1 + num2;
-  result = result.toString();
-
-  for (let i = 0; i < result.length; i++) {
-      arr.push(result[i])
-
-  }
-  arr = arr.reverse()
-
-  return arr;
-
-
-
-}
-
-console.log(addTwoNumbers([2,4,3],[5,6,4]))
 function App() {
-
+  const backgroundStyle = {
+    backgroundImage: `url(${background})`,
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    minHeight: '110vh',  // Ensures that the div is at least as tall as the viewport
+    minWidth: '100%',   // E
+  };
 
   return (
-    <div className="App">
- <Buttons />
+    <div className="App" style={backgroundStyle}>
+      <TopicsPage />
     </div>
   );
 }
---
+++ src/components/SubTopicAccordion.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import { Accordion, AccordionSummary, AccordionDetails, Typography } from '@mui/material';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
+const styles = {
+  accordion: {
+    borderRadius: '20px',
+    backgroundColor: 'rgba(0, 0, 0, 0.7)',
+    transition: 'transform 0.3s ease', // Smooth transition for the transform
+    direction: 'rtl', // Sets text direction to right-to-left
+  },
+  accordionHover: {
+    transform: 'scale(1.05)', // Slightly enlarges the accordion on hover
+  },
+  media: {
+    height: '100%',
+    width: '100%',
+    objectFit: 'cover',
+    margin: '10px',
+    borderRadius: '20px',
+  },
+  content: {
+    fontWeight: 'bold',
+    color: 'white',
+  },
+};
+
+const SubTopicAccordion = ({ title, description, imageUrl }) => {
+  const [hover, setHover] = useState(false);
+
+  return (
+    <Accordion 
+      style={{
+        ...styles.accordion,
+        ...(hover ? styles.accordionHover : null),
+      }}
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+    >
+      <AccordionSummary
+        expandIcon={<ExpandMoreIcon />}
+        aria-controls="panel1a-content"
+        id="panel1a-header"
+      >
+        <img src={imageUrl} alt={title} style={styles.media} />
+      </AccordionSummary>
+      <AccordionDetails>
+        <Typography style={styles.content}>
+          {description}
+        </Typography>
+      </AccordionDetails>
+    </Accordion>
+  );
+};
+
+export default SubTopicAccordion;
